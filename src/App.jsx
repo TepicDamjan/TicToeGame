eb229306cf572e6729db60ef7e4876af0426c04b
Damjan Tepic <damjan.tepic@icloud.com>
2025-01-27 17:34:42 +0100

    TicToe Game
    V1

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Hero } from './pages/Hero'
+import '../src/styles/App.css'
+import { VsCpu } from './pages/vsCpu';
+import { VsPlayer } from './pages/vsPlayer';
+
+function App() {
+
+  return (
+    <Router>
+        <Routes>
+      <Route path="/" element={<Hero />} />
+      <Route path="/vscpu" element={<VsCpu />} />
+      <Route path="/vsplayer" element={<VsPlayer/>} />
+
+    </Routes>
+      </Router>
+    
+  )
+}
+
+export default App
